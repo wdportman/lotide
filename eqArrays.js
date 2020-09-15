a6393da0ea5c1ceab68e8134f29621465b7a552d
Will <wdportman@gmail.com>
2020-09-15 23:57:11 +0000

    Compare two arrays

---
+++ eqArrays.js
@@ -0,0 +1,32 @@
+const eqArrays = function(array1, array2) {
+  let output = "";
+  if (array1.length !== array2.length) {
+    output = false;
+  } else if (array1.length === 0) {
+    output = true;
+  } else {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        output = false;
+      } else {
+        output = true;
+      }
+    }
+  }
+  return output;
+};
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😍😍😍 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🤬🤬🤬 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// TEST CODE
+assertEqual(eqArrays([2, 2], [2, 2]), true);
+assertEqual(eqArrays([2, 2], [2, 3]), false);
+assertEqual(eqArrays([2], [2]), true);
+assertEqual(eqArrays([2], [3]), false);
+assertEqual(eqArrays([], []), true);
