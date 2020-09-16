84c7d16f164d1bac4474f19a750cfa84b4e6489a
Will <wdportman@gmail.com>
2020-09-16 00:09:42 +0000

    Check if two arrays are equal

---
+++ assertArraysEqual.js
@@ -0,0 +1,32 @@
+const eqArrays = function(array1, array2) {
+  let output = "";
+  if (array1.length !== array2.length) {
+    output = false;
+  } else if (array1.length === 0) {
+    output = true;
+  } else {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        output = false;
+      } else {
+        output = true;
+      }
+    }
+  }
+  return output;
+};
+
+const assertArraysEqual = function (array1, array2) {
+  if (eqArrays(array1, array2) === true) {
+    console.log("✅ Yep, the two arrays are equal.");
+  } else {
+    console.log("🚫 No, the two arrays are not equal.");
+  }
+}
+
+//Test cases:
+assertArraysEqual([2, 2], [2, 2]);
+assertArraysEqual([2, 2], [2, 3]);
+assertArraysEqual([2], [2]);
+assertArraysEqual([2], [3]);
+assertArraysEqual([], []);
